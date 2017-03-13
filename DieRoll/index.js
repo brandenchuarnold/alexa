96e215c9d4110d8fc49445be22d99882933285a6
= <brandena@vt.edu>
2017-03-13 17:12:29 -0400

    Die Roll done

---
+++ DieRoll/index.js
@@ -0,0 +1,54 @@
+e strict';
+var Alexa = require('alexa-sdk');
+
+var APP_ID = "amzn1.ask.skill.0de4db33-9de0-446a-9c8e-26aa89b67d1f";
+var SKILL_NAME = 'Die Roll';
+
+/**
+ * Array containing facts.
+ */
+var FACTS = [
+	"One",
+	"Two",
+	"Three",
+	"Four",
+	"Five",
+	"Six"
+];
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('GetFact');
+    },
+    'GetNewFactIntent': function () {
+        this.emit('GetFact');
+    },
+    'GetFact': function () {
+        // Get a random space fact from the die facts list
+        var factIndex = Math.floor(Math.random() * FACTS.length);
+        var randomFact = FACTS[factIndex];
+
+        // Create speech output
+        var speechOutput = "Here's your roll: " + randomFact;
+
+        this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomFact)
+    },
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = "You can say roll, or, you can say exit... What can I help you with?";
+        var reprompt = "What can I help you with?";
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', 'Goodbye!');
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', 'Goodbye!');
+    }
+};
